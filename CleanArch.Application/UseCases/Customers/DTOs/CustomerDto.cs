2b5faec92f05a09810a467f6002727f8f9d629cd
dev.glasdou <dr.glasdou@gmail.com>
2024-01-10 16:05:02 -0600

    Initial Commit

---
+++ CleanArch.Application/UseCases/Customers/DTOs/CustomerDto.cs
@@ -0,0 +1,14 @@
+using CleanArch.Application.UseCases.Common.DTOs;
+using CleanArch.Application.UseCases.Orders.DTOs;
+
+namespace CleanArch.Application.UseCases.Customers.DTOs;
+
+public class CustomerDto : BaseDto
+{
+    public string Name { get; set; }
+}
+
+public class CustomerWithOrdersDto : CustomerDto
+{
+    public List<OrderDto> Orders { get; set; } = new();
+}
